9e24ed7585c335edc150e098f0c4d02d226be35b
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-10-20 15:47:25 -0300

    add fetch-recent-questions and modific core-repositories

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,9 +1,10 @@
-import { Answer } from "../../enterprise/entities/answer";
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Question } from "../../enterprise/entities/question";
 
 export interface QuestionRepository {
   findById(id: string): Promise<Question | null>
   findBySlug(slug: string): Promise<Question | null>
+  findManyRecent(params: PaginationParams): Promise<Question[]>
   save(question: Question): Promise<void>
   create(question: Question): Promise<void>
   delete(question: Question): Promise<void>
